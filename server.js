471476a272e74b385404271c0e1b53423e79a723
Mohamad Safa <mohsafa3@gmail.com>
2023-11-27 14:38:06 +0200

    Packages added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+//const bodyParser = require('body-parser');
+const app = express();
+const connection = require("./config/database");
+const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors()); // Allow all origins for development; adjust for production
+
+//const blogRoutes = require('./routes/route');
+//app.use("/", blogRoutes);
+app.listen(PORT, () => {
+  connection.dbConnection();
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
