0617e5c9d9d9be5fe2b463a6b139b0ea8be03098
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-12-18 16:08:11 +0200

    AddProcduct controller

--- Routes/cartRoute.js
+++ Routes/cartRoute.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const isAuthenticated = require("../middlewares/Auth");
 
 const {
-    getCartByUserID,
-    addProductToCart,
+    // getCartByUserID,
+    // addProductToCart,
+    addProduct,
     updateProductInCart,
 } = require('../Controllers/cartController');
 
-router.get('/getCart/:userID', getCartByUserID);
-router.post('/addProduct/:cartID', isAuthenticated(['user']), addProductToCart);
+// router.get('/getCart/:userID', getCartByUserID);
+// router.post('/addProduct/:cartID', isAuthenticated(['user']), addProductToCart);
+router.post('/addProduct', addProduct);
 router.put('/updateProduct/:cartID', isAuthenticated(['user']), updateProductInCart);
 
 module.exports = router;
