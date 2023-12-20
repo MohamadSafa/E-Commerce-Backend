f508896e4a90dba522b0fa4c0d06d0650dfaba6b
Mohamad Safa <mohsafa3@gmail.com>
2023-12-20 12:40:59 +0200

    Auth Adjusted

--- Routes/cartRoute.js
+++ Routes/cartRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const isAuthenticated = require("../middlewares/auth");
+// const isAuthenticated = require("../middlewares/Auth");
 
 const {
     getCartByUserID,
